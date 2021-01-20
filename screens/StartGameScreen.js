6a194cc67713ce6e6944bad5bcdd890f221b8eec
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-01-20 12:18:53 +0500

    Cleaning User Input & controlling Soft Keyboard

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,16 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Card from '../components/Card';
 import Colors from '../constants/colors';
 import Input from  '../components/input';
 
 const StartGameScreen = props => {
+
+    const [enteredValue, setEnteredValue] =useState('');
+    const numberInputHandler = inputText => {
+        setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+    };
     return (
+        <TouchableWithoutFeedback onPress={()=> {
+            Keyboard.dismiss();
+        }}>
         <View style={styles.screen}>
             <Text>Start a New Game!</Text>
             <Card style={styles.inputContainer}>
                 <Text style={styles.title}>Select a Number</Text>
-                <Input style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false} keyboardType="number-pad" maxLength={2} />
+                <Input 
+                style={styles.input} 
+                blurOnSubmit autoCapitalize='none' 
+                autoCorrect={false} 
+                keyboardType="number-pad" 
+                maxLength={2} 
+                onChangeText = {numberInputHandler}
+                value = {enteredValue}
+                />
                 <View style={styles.buttonContainer}>
                    <View style={styles.button}> 
                    <Button title="Reset" onPress={() => {}} color={Colors.accent} />
@@ -21,6 +37,7 @@ const StartGameScreen = props => {
                 </View>
             </Card>
         </View>
+        </TouchableWithoutFeedback>
     )
 }
 
