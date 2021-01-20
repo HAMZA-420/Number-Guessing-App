033d6f44c88bcb4ca8c111ed701a5e670098c6e6
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-01-20 09:37:54 +0500

    adding components

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const StartGameScreen = props => {
+    return (
+        <View style={styles.screen}>
+            <Text>Start a New Game!</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center'
+    }
+})
+
+export default StartGameScreen;
